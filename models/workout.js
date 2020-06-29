92407858b9dd3ee7c4e89bfc6f23f278ab3bb393
deawar <deawar@gmail.com>
2020-06-29 17:59:50 -0400

    Early Work

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+	{
+		day: {
+			type: Date,
+			default: () => new Date()
+		},
+		exercises: [
+			{
+					type:{
+						type: String,
+						trim: true,
+						required: "Please enter an exercise Type"
+					},
+					name: {  
+						type: String,
+						trim: true,
+						required: "Please enter an exercise Name"
+					},
+					duration: {
+						type: Number,
+						trim: true,
+						required: "Please enter an exercise Duration"
+					},
+					weight:{
+						type: Number,
+					},
+					reps:{
+						type: Number,
+					},
+					sets:{
+						type: Number,
+					},
+					distance:{
+						type: Number,
+					},
+				}
+		]
+	}
+)
+
+const Workout = mongoose.model("Workout", workoutSchema);
+modules.exports = Workout;
