db0c39cf3f3281abc70934432c0c17a33b84f231
deawar <deawar@gmail.com>
2020-06-30 14:47:51 -0400

    Updates to stats routes

--- public/workout.js
+++ public/workout.js
@@ -22,11 +22,13 @@ async function initWorkout() {
 function tallyExercises(exercises) {
   const tallied = exercises.reduce((acc, curr) => {
     if (curr.type === "resistance") {
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
       acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
       acc.totalSets = (acc.totalSets || 0) + curr.sets;
       acc.totalReps = (acc.totalReps || 0) + curr.reps;
     } else if (curr.type === "cardio") {
       acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
+      acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
     }
     return acc;
   }, {});
